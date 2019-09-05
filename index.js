6ba97de0de49413f64ca081b1c3dbf465e858873
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-09-05 06:26:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,48 @@
+let store = {drivers: [], passengers: [], trips: []}
+
+let driverId = 0;
+let tripId = 0;
+let passengerId = 0;
+
+class Driver {
+  constructor(name) {
+    this.name = name;
+    this.id = ++driverId;
+    store.drivers.push(this);
+  }
+  trips() {
+    return store.trips.filter(trip => trip.driverId === this.id);
+  }
+  passengers() {
+    return this.trips().map(trip => trip.passenger());
+  }
+}
+
+class Passenger {
+  constructor(name) {
+    this.name = name;
+    this.id = ++passengerId;
+    store.passengers.push(this);
+  }
+  trips() {
+    return store.trips.filter(trip => trip.passengerId === this.id);
+  }
+  drivers() {
+     return this.trips().map(trip => trip.driver());
+  }
+}
+
+class Trip {
+  constructor(driver, passenger){
+      this.id = ++tripId;
+      this.driverId = driver.id;
+      this.passengerId = passenger.id;
+      store.trips.push(this);
+  }
+  driver() {
+    return store.drivers.filter(driver => driver.id === this.driverId)[0];
+  }
+  passenger() {
+    return store.passengers.filter(passenger => passenger.id === this.passengerId)[0];
+  }
+}
